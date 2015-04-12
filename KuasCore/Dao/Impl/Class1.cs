8a2c6a5edc21e1264db1903782bf64bc48d2cf08
1101137118 <r061115@gmail.com>
2015-04-12 21:39:28 +0800

    https://github.com/1101137118/HOMEWORK-Sprint.NET-IOC.git

---
+++ KuasCore/Dao/Impl/Class1.cs
@@ -0,0 +1,48 @@
+﻿
+using KuasCore.Dao.Base;
+using KuasCore.Dao.Mapper;
+using KuasCore.Models;
+using Spring.Data.Common;
+using Spring.Data.Generic;
+using System.Collections.Generic;
+using System.Data;
+
+namespace KuasCore.Dao.Impl
+{
+    public class Class1 : IEmployeeDao
+    {
+
+        public IList<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee1 = new Employee();
+            employee1.Id = "GSS";
+            employee1.Name = "叡揚資訊";
+            employee1.Age = 1;
+            employees.Add(employee1);
+
+            Employee employee2 = new Employee();
+            employee2.Id = "11";
+            employee2.Name = "1";
+            employee2.Age = 1;
+            employees.Add(employee2);
+            return employees;
+        }
+
+        public Employee GetEmployeeById(string id)
+        {
+            Employee employee = null;
+
+
+            employee = new Employee();
+            employee.Id = "11";
+            employee.Name = "1";
+            employee.Age = 1;
+
+
+            return employee;
+        }
+
+    }
+}
---
+++ KuasCore/Services/Impl/EmployeeService.cs
@@ -0,0 +1,34 @@
+﻿using KuasCore.Dao;
+using KuasCore.Dao.Impl;
+using KuasCore.Models;
+using System.Collections.Generic;
+
+namespace KuasCore.Services.Impl
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        public IEmployeeDao EmployeeDao = new EmployeeDao();
+
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeDao.GetAllEmployees();
+        }
+
+        public Employee GetEmployeeById(string id)
+        {
+            Employee employee = null;
+
+            if ("1".Equals(id))
+            {
+                employee = new Employee();
+                employee.Id = "1";
+                employee.Name = "1";
+            }
+
+            return employee;
+        }
+
+    }
+
+}
